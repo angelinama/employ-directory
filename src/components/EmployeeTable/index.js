19bd91c9869403ad7a0609a9c99f6bbe2db3d483
Bingqing(Angelina) Ma <angelina890308@gmail.com>
2021-03-20 01:48:06 -0700

    deploy changes

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -2,6 +2,7 @@ import React from "react";
 // import "./style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSortAlphaDown } from "@fortawesome/free-solid-svg-icons";
+import Col from "../Col";
 
 function EmployeeTable(props) {
   //TODO change this whole function by using hooks
@@ -21,47 +22,49 @@ function EmployeeTable(props) {
   };
 
   return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">Photo</th>
-          <th scope="col">
-            First{" "}
-            <span onClick={handleSort} style={{ opacity: 0.33 }} id="first">
-              <FontAwesomeIcon icon={faSortAlphaDown} />
-            </span>
-          </th>
-          <th scope="col">
-            Last{" "}
-            <span onClick={handleSort} style={{ opacity: 0.33 }} id="last">
-              <FontAwesomeIcon icon={faSortAlphaDown} />
-            </span>
-          </th>
-          <th scope="col">Email</th>
-          <th scope="col">Phone</th>
-          <th scope="col">Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.employees.map((employee) => (
-          <tr key={employee.name.first + " " + employee.name.last}>
-            <td>
-              <img
-                alt={employee.name.first + " " + employee.name.last}
-                src={employee.picture.large}
-                className="img-fluid"
-              />
-            </td>
-            <td>{employee.name.first}</td>
-            <td>{employee.name.last}</td>
-            <td>{employee.email}</td>
-            {/* TODO:  format cell value for better display */}
-            <td>{employee.cell}</td>
-            <td>{`${employee.location.city}, ${employee.location.state}, ${employee.location.country}`}</td>
+    <Col size="md-12">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Photo</th>
+            <th scope="col">
+              First{" "}
+              <span onClick={handleSort} style={{ opacity: 0.33 }} id="first">
+                <FontAwesomeIcon icon={faSortAlphaDown} />
+              </span>
+            </th>
+            <th scope="col">
+              Last{" "}
+              <span onClick={handleSort} style={{ opacity: 0.33 }} id="last">
+                <FontAwesomeIcon icon={faSortAlphaDown} />
+              </span>
+            </th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Location</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {props.employees.map((employee) => (
+            <tr key={employee.name.first + " " + employee.name.last}>
+              <td>
+                <img
+                  alt={employee.name.first + " " + employee.name.last}
+                  src={employee.picture.large}
+                  className="img-fluid"
+                />
+              </td>
+              <td>{employee.name.first}</td>
+              <td>{employee.name.last}</td>
+              <td>{employee.email}</td>
+              {/* TODO:  format cell value for better display */}
+              <td>{employee.cell}</td>
+              <td>{`${employee.location.city}, ${employee.location.state}, ${employee.location.country}`}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Col>
   );
 }
 
